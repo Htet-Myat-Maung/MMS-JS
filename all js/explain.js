6d45a2d1590f03aaf9831bc7ab64599b03aa4550
Htet Myat Maung <riro263232@gmail.com>
2024-11-22 15:43:42 +0630

    Day-5 Complete

---
+++ all js/explain.js
@@ -0,0 +1,55 @@
+
+//stack memory
+// let x = 10;
+// const y = 15;
+
+//heap memory
+// let z = ["a", "b", "c"];
+// const run = function () {
+//     return "The explain"
+// };
+
+
+
+//stack exp
+// let x = 10;
+// console.log(x);
+
+// let y = x;
+// console.log(y);
+
+// x = 15;
+// console.log(x);
+// console.log(y);
+
+//Heap exp
+// const  x = ["a", "b", "c"];
+// console.log(x);
+
+// let y = x;
+// console.log(y);
+
+// x[2] = "kkk";
+// console.log(x);
+// console.log(y);
+
+// let x = ["a", "b", "c"];
+// let y = x;
+// console.log(y);
+
+// x = ["d", "e", "f"];
+// console.log(y);
+
+
+// let x = 5;
+// let y = 5;
+// console.log(x === y);
+
+// let x = []; 
+// let y = [];
+// console.log(x === y); //array address မတူလို့ false ထွက်,
+
+// const a = {}
+// const b = {};
+// console.log(a === b); //same as array
+
--- all js/obj.js
+++ all js/obj.js
@@ -1,17 +1,17 @@
-const obj = {
+// const obj = {
     //properties
-    a : 5,
-    b : 10,
-    xyz : 100,
+//     a : 5,
+//     b : 10,
+//     xyz : 100,
     //methods
-    run : function () {
-        console.log(this);
-        return "run method";
-    },
-    test() {
-        return `test method, ${this.a}, ${this.xyz}`;
-    }
-};
-console.log(obj);
-console.log(obj.run());
-console.log(obj.test());
+//     run : function () {
+//         console.log(this);
+//         return "run method";
+//     },
+//     test() {
+//         return `test method, ${this.a}, ${this.xyz}`;
+//     }
+// };
+// console.log(obj);
+// console.log(obj.run());
+// console.log(obj.test());

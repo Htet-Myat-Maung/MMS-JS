64ea92e4c64ed3050d446f29107b4b1805166c5a
Htet Myat Maung <riro263232@gmail.com>
2024-11-28 19:39:58 +0630

    delete in typeOf this

--- all js/switch_while.js
+++ all js/switch_while.js
@@ -1,4 +1,4 @@
-console.log("switch", "while", "do...while");
+console.log("switch", "while", "do...while", "delete", "in", "typeOf", "this");
 
 // const x = 3;
 
@@ -33,3 +33,45 @@ console.log("switch", "while", "do...while");
 //   console.log("* * *");
 //   x++;
 // } while (x <= 5);
+
+// let person = {
+//   firstName: "htet myat",
+//   lastName: "maung",
+//   age: 21,
+// };
+
+// delete person.lastName;
+
+// console.log(person);
+
+// const car = { make: "Honda", model: "Accord", year: 1998 };
+// console.log(car);
+// console.log("make" in car);
+
+// delete car.make;
+// console.log(car);
+
+// if ("make" in car === false) {
+//   car.make = "BMW";
+// }
+
+// console.log(car.make);
+
+// let test = function (x) {
+//   return "Hello " + x;
+// };
+// console.log(test("world"));
+
+// console.log(typeof {});
+// console.log(typeof []);
+// console.log(typeof "");
+// console.log(typeof 0);
+// console.log(typeof null);
+
+const test = {
+  num: 2,
+  fun: function () {
+    return this.num;
+  },
+};
+console.log(test.fun());
